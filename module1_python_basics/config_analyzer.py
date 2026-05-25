49f6d55e718bfeab3fbf4af40b7f902abd6e0e29
ritikasi <ritika.v02@gmail.com>
2026-05-25 23:30:09 +0530

    module1 session2: extract interfaces and IP addresses from config

--- module1_python_basics/config_analyzer.py
+++ module1_python_basics/config_analyzer.py
@@ -4,3 +4,18 @@ with open(show_run, "r") as f:
         if line.strip().startswith("hostname"):
             x = line.strip().split()[1]
             print(x)
+
+with open(show_run, "r") as f:
+    y = f.readlines()
+    for i, line in enumerate(y):
+        if line.strip().startswith("interface"):
+            for j in range(i+1, i+10):
+                if "ip address" in y[j] and "no ip address" not in y[j]:
+                    ip = y[j].strip().split()[2]
+                    mask = y[j].strip().split()[3]
+                    print(line.strip(), ip, mask)
+                    break
+                if y[j].strip() == "!":
+                    break
+
+
