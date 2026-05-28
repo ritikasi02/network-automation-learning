59aea36e6d92346cfea88255451f1d75f981506e
ritikasi <ritika.v02@gmail.com>
2026-05-28 23:37:47 +0530

    module1 session5: parse OSPF process and networks with regex

--- module1_python_basics/config_analyzer.py
+++ module1_python_basics/config_analyzer.py
@@ -19,6 +19,8 @@
 #                    break
 
 import os
+import re
+
 
 def read_confg(filepath):
     with open(filepath, "r") as f:
@@ -43,9 +45,27 @@ def get_interfaces(lines):
                     break
     return interface
 
+def get_ospf(lines):
+    ospf = []
+    for i, line in enumerate(lines):
+        if line.strip().startswith("router ospf"):
+            for j in range(i+1, i+10):
+                match = re.search(r"network (\S+) (\S+) area (\d+)", lines[j])
+                if match:
+                    network_ip = match.group(1)
+                    wildcard = match.group(2)
+                    area = match.group(3)
+                    ospf.append({"network": network_ip, "wildcard": wildcard, "area": area})
+                if lines[j].strip() == '!':
+                    break
+    return ospf
+
+
+
+
 if __name__ == "__main__":
     folder = "sample_configs"
-    config_files = os.listdir(folder)
+    config_files = sorted(os.listdir(folder))
     for filename in config_files:
         filepath = os.path.join(folder, filename)
         lines =read_confg(filepath)
@@ -54,6 +74,9 @@ if __name__ == "__main__":
         print(hostname)
         for intf in interface:
             print(f" {intf['name']} {intf['ip']} {intf['mask']}")
+        ospf = get_ospf(lines)
+        for c in ospf:
+            print(f" network {c['network']} {c['wildcard']} area {c['area']}" )
 
 
 
